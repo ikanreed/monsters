69e12c80b19fa5a3f0802e3522480ccabefdfaff
Christopher Ashley Cline <grumpusbumpus@gmail.com>
2016-07-30 17:25:16 -0400

    Sketched out monster fields
    First pass at declaring the monster model

--- monsters/monsterlibrary/models.py
+++ monsters/monsterlibrary/models.py
@@ -5,4 +5,30 @@ from django.db import models
 # Create your models here.
 
 define Monster(models.Model)
+	name = models.CharField()
+	# Size should be its own table
+	# Type comes from a table
+	# Alignment is going to come from a table
+	# Armor Class : Haven't decided how this works
+	# Hit Points are derived
+	hit_dice = models.IntegerField() # May not be correct class callable
+	# Speed has its own table too
+	strength = models.IntegerField()
+	dexterity = models.IntegerField()
+	constitution = models.IntegerField()
+	intelligence = models.IntegerField()
+	wisdom = models.IntegerField()
+	charisma = models.IntegerField()
+	# damage_resistances
+	# damage_immunities
+	# condition_immunities
+	# senses
+	# languages
+	# skills
+	# challenge = derived
+	# POWERS = class
+	# ACTIONS
+	# LEGENDARY ACTIONS
+	# LAIR ACTIONS
+	
 	
